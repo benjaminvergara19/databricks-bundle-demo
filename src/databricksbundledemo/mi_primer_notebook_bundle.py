bc2fad1a0b82c8dec504037fcd8a816a0b21c37f
benjamin.vergara <benjamin.vergara@uc.cl>
2025-08-11 15:31:54 -0400

    [deploy] probando si funciona las validaciones segunda oportunidad

--- src/databricksbundledemo/mi_primer_notebook_bundle.py
+++ src/databricksbundledemo/mi_primer_notebook_bundle.py
@@ -3,7 +3,7 @@
 # Este es un notebook que será desplegado y ejecutado
 # a través de un Databricks Asset Bundle.
 
-print("¡Hola Mundo desde un Bundle de Databricks! jiji")
+print("¡Hola Mundo desde un Bundle de Databricks! jiji probando validaciones")
 
 # COMMAND ----------
 
