0fe88bba53e2b6ac3e66d2a297c8d14d7e1452ca
benjamin.vergara <benjamin.vergara@uc.cl>
2025-08-11 15:36:30 -0400

    sin el cosito

--- src/databricksbundledemo/mi_primer_notebook_bundle.py
+++ src/databricksbundledemo/mi_primer_notebook_bundle.py
@@ -3,7 +3,8 @@
 # Este es un notebook que será desplegado y ejecutado
 # a través de un Databricks Asset Bundle.
 
-print("¡Hola Mundo desde un Bundle de Databricks! jiji probando validaciones")
+print("¡Hola Mundo desde un Bundle de Databricks! jiji probando que la validacion funcione" \
+"dandole algo equivocado")
 
 # COMMAND ----------
 
